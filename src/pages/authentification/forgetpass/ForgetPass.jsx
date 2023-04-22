fcad201cf0a36b5302163ef0ecb0bd19d800d27d
EKIA Benjamin <ekiabenjamin@gmail.com>
2023-04-22 17:53:32 +0200

    mise a jour de la page login, forgetPass et register

--- src/pages/authentification/forgetpass/ForgetPass.jsx
+++ src/pages/authentification/forgetpass/ForgetPass.jsx
@@ -5,36 +5,44 @@ import "../../../components/footer/Footer.css"
 
 const ForgetPass = () => {
   return (
+
     <div className="container">
       <div className='container-card'>
         <div className="card-content">
             <div className='block'>
                 <h1>Instagram</h1>
                 <p>Problèmes de connexion</p>
-                
-                
             </div>
+
             <div>
                   <p>Entrez votre adresse e-mail, votre numéro de téléphone 
                     ou votre nom d’utilisateur, et nous vous enverrons un 
                     lien pour récupérer votre compte.
                   </p>
             </div>
-          <input type="text" id="name" placeholder="E-mail, telephone ou nom d'ulitisateur "></input>
-          <div className="space-btn">
-            <button className="btn-color">Envoyer un lien ou nom d'utilisateur</button>
-          </div>
-          <div className="space-btn">
-            <NavLink to="/forgetpass"><p>Vous ne parvenez pas à reintiliser votre mot de passe ?</p></NavLink>
-          </div>
-          <div className="space-btn">
+
+            <form action="#" className="signup">
+                  <div className="field">
+                     <input type="text" placeholder="E-mail, telephone ou nom d'ulitisateur" required />
+                  </div>
+                  <div className="field btn">
+                     <input className="btn-layer" type="submit" value="Envoyer un lien ou nom d'utilisateur" />
+                  </div>
+            </form>
+            
+
+            <div className="space-btn">
+              <NavLink  className='nav-link' to="/forgetpass"><p className='style-link'>Vous ne parvenez pas à reintiliser votre mot de passe ?</p></NavLink>
+            </div>
+            <div className="space-btn">
+            <hr />
             <p>Créer un compte</p>
           </div>
         </div>
       </div>
       <div className='container-card-child'>
         <div className="card-content">
-        <p>Vous n'avez pas de compte ?<NavLink className="sp-color" to="/register">Inscrivez-vous</NavLink> </p>
+          <p>Vous n'avez pas de compte ? <NavLink className="sp-color" to="/register">Inscrivez-vous</NavLink> </p>
         </div>
       </div>  
     </div>
--- src/pages/authentification/register/Register.jsx
+++ src/pages/authentification/register/Register.jsx
@@ -14,27 +14,38 @@ const Register = () => {
                   et vidéos de vos amis.
                 </p>
             </div>
-          <div className="space-btn">
-          <button className="btn-color">Se connecter avec Facebook</button>
-          </div>
-          <div className='dp-input'>
-          <input type="text" id="name" placeholder="Numéro de mobile ou e-mail"></input>
-          <input type="password" id="name" placeholder="Nom complet"></input>
-          <input type="text" id="name" placeholder="Nom d'ulitisateur"></input>
-          <input type="password" id="name" placeholder="Mot de passe"></input>
-          </div>
+            <div className="field btn">
+                <input className="btn-layer" type="submit" value="Se connecter avec Facebook" />
+            </div>
+          
+            <form action="#" className="signup">
+                    <div className="field">
+                      <input type="text" placeholder="E-mail, telephone ou nom d'ulitisateur" required />
+                    </div>
+                    <div className="field">
+                      <input type="text" placeholder="E-mail, telephone ou nom d'ulitisateur" required />
+                    </div>
+                    <div className="field">
+                      <input type="text" placeholder="E-mail, telephone ou nom d'ulitisateur" required />
+                    </div>
+                    <div className="field">
+                      <input type="text" placeholder="E-mail, telephone ou nom d'ulitisateur" required />
+                    </div>
+              </form>
          
-          <div className="space-btn">
-          <NavLink to="/forgetpass"><p>Mot de passe oublié ?</p></NavLink>
-            <p>Les personnes qui utilisent notre service ont pu importer vos 
-              coordonnées sur Instagram. En savoir plus
-            </p>
-            <p>En vous inscrivant, vous acceptez nos Conditions générales. 
-              Découvrez comment nous recueillons, utilisons et partageons 
-              vos données en lisant notre Politique de confidentialité et comment nous utilisons les cookies et autres technologies similaires en consultant notre Politique d’utilisation des cookies.
-            </p>
-            <button className="btn-color">Suivant</button>
-          </div>
+              <div className="space-btn">
+                  <NavLink to="/forgetpass"><p>Mot de passe oublié ?</p></NavLink>
+                    <p>Les personnes qui utilisent notre service ont pu importer vos 
+                      coordonnées sur Instagram. En savoir plus
+                    </p>
+                    <p>En vous inscrivant, vous acceptez nos Conditions générales. 
+                      Découvrez comment nous recueillons, utilisons et partageons 
+                      vos données en lisant notre Politique de confidentialité et comment nous utilisons les cookies et autres technologies similaires en consultant notre Politique d’utilisation des cookies.
+                    </p>
+                    <div className="field btn">
+                        <input className="btn-layer" type="submit" value="Suivant" />
+                    </div>
+                </div>
         </div>
       </div>
 
